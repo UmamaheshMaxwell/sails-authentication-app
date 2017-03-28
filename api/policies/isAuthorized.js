0527ea401cc7c3ad3a635c959efe0eb53e9744df
UmamaheswararaoMeka <maheshmeka16@gmail.com>
2017-03-28 16:54:26 +0530

    Formatted authorize file

--- api/policies/isAuthorized.js
+++ api/policies/isAuthorized.js
@@ -6,31 +6,33 @@
 
 
 module.exports = function (req, res, next) {
-  var token;
+	var token;
 
-  if (req.headers && req.headers.authorization) {
-    var parts = req.headers.authorization.split(' ');
-    if (parts.length == 2) {
-      var scheme = parts[0],
-        credentials = parts[1];
+	if (req.headers && req.headers.authorization) {
+		var parts = req.headers.authorization.split(' ');
+		if (parts.length == 2) {
+			var scheme = parts[0],
+				credentials = parts[1];
 
-      if (/^Bearer$/i.test(scheme)) {
-        token = credentials;
-      }
-    } else {
-      return res.json(401, {err: 'Format is Authorization: Bearer [token]'});
-    }
-  } else if (req.param('token')) {
-    token = req.param('token');
-    // We delete the token from param to not mess with blueprints
-    delete req.query.token;
-  } else {
-    return res.json(401, {err: 'No Authorization header was found'});
-  }
+			if (/^Bearer$/i.test(scheme)) {
+				token = credentials;
+			}
+		} else {
+				return res.json(401, {err: 'Format is, Authorization: Bearer [token]'});
+		}
+	} else if (req.param('token')) {
+		token = req.param('token');
+		// We delete the token from param to not mess with blueprints
+		delete req.query.token;
+	} else {
+		return res.json(401, {err: 'No Authorization header was found'});
+	}
 
-  jwToken.verify(token, function (err, token) {
-    if (err) return res.json(401, {err: 'Invalid Token!'});
-    req.token = token; // This is the decrypted token or the payload you provided
-    next();
-  });
+	jwToken.verify(token, function (err, token) {
+		if (err) {
+			return res.json(401, {err: 'Invalid Token!'});
+		}
+		req.token = token; 
+		next();
+	});
 };
